de38b87b78ec820ecadd3a3ca93171f1d9508a4d
EmanuelBatixta <emanueljonatas28@gmail.com>
2025-11-21 22:11:05 -0300

    fix: correct import path for setupSwagger in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupSwagger } from 'swagger.config';
+import { setupSwagger } from '../swagger.config';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
